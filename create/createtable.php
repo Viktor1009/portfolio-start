a223ccfa3bd20b1da9e302434e3c994853e12a16
Viktor1009 <viktorosterberg10@gmail.com>
2025-11-10 12:02:08 +0100

    addede insert into aswell as adding the creation of users everytime you make a new database

--- create/createtable.php
+++ create/createtable.php
@@ -1,41 +1,50 @@
 <?php
-        $sql = "CREATE TABLE IF NOT EXISTS Project (
+
+        displayMsg("success", "starting creation of tables");
+
+        $sql = "CREATE TABLE IF NOT EXISTS project (
         project_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
         project_name VARCHAR(50) NOT NULL,
         project_info VARCHAR(500) NOT NULL,
         project_link VARCHAR(100),
         project_thumbnail VARCHAR(100)
         )";
+        $createProjects = $conn->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS Image (
+        $sql = "CREATE TABLE IF NOT EXISTS image (
         project_id INT NOT NULL,
         image_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
         image_url VARCHAR(100) NOT NULL
         )";
+        $createImages = $conn->query($sql);
         
-        $sql = "CREATE TABLE IF NOT EXISTS Categories (
+        $sql = "CREATE TABLE IF NOT EXISTS categories (
         cat_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
         cat_name VARCHAR(100) NOT NULL
         )";
+        $createCatergories = $conn->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS Categories_Relations (
+        $sql = "CREATE TABLE IF NOT EXISTS categories_relations (
         cat_id INT NOT NULL,
         project_id INT NOT NULL
         )";
+        $createCatergories_Relations = $conn->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS Info (
-        person_name VARCHAR(50) NOT NULL
-        person_phone VARCHAR(50)
-        person_mail VARCHAR(100)
-        person_discord VARCHAR(100)
-        person_about VARCHAR(500)
-        person_image VARCHAR(100)
+        $sql = "CREATE TABLE IF NOT EXISTS info (
+        person_name VARCHAR(50) NOT NULL,
+        person_phone VARCHAR(50),
+        person_mail VARCHAR(100),
+        person_discord VARCHAR(100),
+        person_about VARCHAR(500),
+        person_image VARCHAR(100),
         person_welcome VARCHAR(500)
-        )";
+        )"; 
+        $createInfo = $conn->query($sql);
 
-        $sql = "CREATE TABLE IF NOT EXISTS Users (
-        login_username VARCHAR(100)
-        login_password VARCHAR(100)
+        $sql = "CREATE TABLE IF NOT EXISTS users (
+        login_username VARCHAR(100),
+        login_password VARCHAR(100),
         login_role VARCHAR(100)
         )";
+        $createUsers = $conn->query($sql);
     ?>
--- installscript.php
+++ installscript.php
@@ -4,17 +4,18 @@
     if(!$conn) {
         displayMsg("error", "Wrong passowrd for database");
         exit();
-    } else {
+    } 
+    else {
         displayMsg("success", "Connected");
 
-        $sql = "CREATE DATABASE" . $_POST["dbname"];
+        $sql = "CREATE DATABASE " . $_POST["dbname"];
         try {
             $conn->query($sql);
-            displayMsg ("success", "Database created successfully")
-        }catch(mysqli_sql_exeception $exeption){
-            displayMsg("error", "Database already exists")
+            displayMsg ("success", "Database created successfully");
         }
-
+        catch(mysqli_sql_exeception $exception){
+            displayMsg("error", "Database already exists");
+        }
     }
     $conn->close();
     $conn = mysqli_connect($_POST["host"], $_POST["dbuser"], $_POST["dbpass"], $_POST["dbname"]);
@@ -25,14 +26,40 @@
     } else {
         displayMsg("success", "Make Tables");
     
-        include_once("../../create/createtable.php")
+        include_once("../../create/createtable.php");
     }
     $conn->close();
     $conn = mysqli_connect($_POST["host"], $_POST["dbuser"], $_POST["dbpass"], $_POST["dbname"]);
+
+    if(!$conn) {
+        displayMsg("error", "idk man");
+    }
+    else {
+        $conn->query("INSERT INTO project (project_name, project_info) VALUES ('testproject', 'testinfo')");
+        $conn->query("INSERT INTO info (person_name, person_mail, person_about, person_welcome) VALUES ('testname', 'test.test@test.test', 'testing the about section', 'welcome!!!')");
+    }
+    $conn->close();
+    $conn = mysqli_connect($_POST["host"], $_POST["dbuser"], $_POST["dbpass"], $_POST["dbname"]);
+    
+    if(!$conn) {
+        displayMsg("error", "this is the Users");
+    }
+    else {
+            $stmt = $conn->prepare("INSERT INTO users(login_username, login_password) VALUES(?,?)");
+            $stmt->bind_param("ss", $_POST["username"], $_POST["password"]);
+            $stmt->execute();
+            $stmt->close();
+
+            $conn->query("INSERT INTO users (login_role) VALUES ('admin')");
+    }
+    $conn->close();
+
+    // $conn->query("INSERT INTO pizzas (name, type, cost) VALUES ('testPizza', 'testToppings', 1)");
+    
     /**
      * Här vill jag att ni fortsätter, ni ska
-     * 1. Skapa en databas.
-     * 2. Skapa tabeller
+     * 1. Skapa en databas. -
+     * 2. Skapa tabeller -
      * 3. Lägga in eventuell dummy data som behövs direkt
      * 4. Skapa en användare i users-tabellen
      * 4. Skapa en .env som vi använder i fortsättningen.
