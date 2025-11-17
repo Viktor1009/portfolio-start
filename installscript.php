18831ca3a459e708033539f09cf1340a21b7f86f
Viktor1009 <viktorosterberg10@gmail.com>
2025-11-17 12:20:49 +0100

    har skapat env och conn. har återrättat dash. fixade dummy datan i installscript

--- installscript.php
+++ installscript.php
@@ -1,5 +1,4 @@
 <?php
-
     $conn = mysqli_connect($_POST["host"], $_POST["dbuser"], $_POST["dbpass"]);
     if(!$conn) {
         displayMsg("error", "Wrong passowrd for database");
@@ -8,7 +7,7 @@
     else {
         displayMsg("success", "Connected");
 
-        $sql = "CREATE DATABASE " . $_POST["dbname"];
+        $sql = "CREATE DATABASE IF NOT EXISTS " . $_POST["dbname"];
         try {
             $conn->query($sql);
             displayMsg ("success", "Database created successfully");
@@ -26,43 +25,44 @@
     } else {
         displayMsg("success", "Make Tables");
     
-        include_once("../../create/createtable.php");
-    }
-    $conn->close();
-    $conn = mysqli_connect($_POST["host"], $_POST["dbuser"], $_POST["dbpass"], $_POST["dbname"]);
+        include_once("../../template/createtable.php");
 
-    if(!$conn) {
-        displayMsg("error", "idk man");
-    }
-    else {
         $conn->query("INSERT INTO project (project_name, project_info) VALUES ('testproject', 'testinfo')");
-        $conn->query("INSERT INTO info (person_name, person_mail, person_about, person_welcome) VALUES ('testname', 'test.test@test.test', 'testing the about section', 'welcome!!!')");
-    }
-    $conn->close();
-    $conn = mysqli_connect($_POST["host"], $_POST["dbuser"], $_POST["dbpass"], $_POST["dbname"]);
-    
-    if(!$conn) {
-        displayMsg("error", "this is the Users");
-    }
-    else {
-            $stmt = $conn->prepare("INSERT INTO users(login_username, login_password) VALUES(?,?)");
-            $stmt->bind_param("ss", $_POST["username"], $_POST["password"]);
-            $stmt->execute();
-            $stmt->close();
+        $conn->query("INSERT INTO info (person_name, person_mail, person_about, person_welcome) VALUES 
+                    ('testname', 'test.test@test.test', 'testing the about section', 'welcome!!!')");
 
-            $conn->query("INSERT INTO users (login_role) VALUES ('admin')");
+        $admin = "admin";
+        $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+        $stmt = $conn->prepare("INSERT INTO user(login_username, login_password, login_role) VALUES(?,?,?)");
+        $stmt->bind_param("sss", $_POST["admin"], $password, $admin);
+        $stmt->execute();
+        $stmt->close();
+
+        makeEnv();
     }
     $conn->close();
 
-    // $conn->query("INSERT INTO pizzas (name, type, cost) VALUES ('testPizza', 'testToppings', 1)");
+    function makeEnv(){
+        $env = [
+            'DB_HOST' => $_POST["host"],
+            'DB_PORT' => '3306',
+            'DB_DATABASE' => $_POST["dbname"],
+            'DB_USER' => $_POST["dbuser"],
+            'DB_PASSWORD' => $_POST["dbpass"],
+        ];
+        $content = "";
+        foreach ($env as $key => $value) {
+            $content .= "{$key}={$value}\n";
+        }    
     
-    /**
-     * Här vill jag att ni fortsätter, ni ska
-     * 1. Skapa en databas. -
-     * 2. Skapa tabeller -
-     * 3. Lägga in eventuell dummy data som behövs direkt
-     * 4. Skapa en användare i users-tabellen
-     * 4. Skapa en .env som vi använder i fortsättningen.
-     * https://www.w3schools.com/php/php_mysql_create_table.asp
-     */
+        $file = __DIR__ . '/.env';
+        if (file_put_contents($file, $content)) {
+            displayMsg("success", "All done");
+        } else {
+            echo "Något knas";
+        }
+
+        echo '<a href="/"> Gå tillbaka</a>';
+    }
 ?>
